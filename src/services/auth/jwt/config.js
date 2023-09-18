3d2b5cec3ec71bf5704e19d2ac025ffb4c440ff4
gfarias <gustavo.farias.e@gmail.com>
2023-09-17 23:06:56 -0300

    legacy

--- src/services/auth/jwt/config.js
+++ src/services/auth/jwt/config.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const BASE_URL = process.env.REACT_APP_API_URL;
 
 export default axios.create({
-  baseURL: BASE_URL, //'http://localhost:3005/api/', //`http://g-axon.work/jwtauth/api/`, //YOUR_API_URL HERE
+  baseURL: 'https://cartones-api.azurewebsites.net/api/', //'http://localhost:3005/api/', //`http://g-axon.work/jwtauth/api/`, //YOUR_API_URL HERE
   headers: {
     'Content-Type': 'application/json',
   },
